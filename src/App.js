5b9a0e34c5891e5f9babfd2b19ec2707cb47c42d
Christopher Means <means2014@gmail.com>
2020-01-17 15:51:25 -0500

    edited App.js to satisfy test case

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-		  Ok, bruv
         </p>
-        <p>
-			Test gonna fail, now bruv.
-		</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
